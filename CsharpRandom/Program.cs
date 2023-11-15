f514aecc805578551cc69a8b6bbd1b0a8b0c2c06
ivarmar <141323294+ivarmar@users.noreply.github.com>
2023-11-15 14:51:26 +0100

    Add project files.

---
+++ CsharpRandom/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CsharpRandom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomGenerator hobbyInstance = new RandomGenerator();
+
+            while (true)
+            {
+                Console.WriteLine("Enter your name for a hobby (or type 'exit' to end):");
+                var name = Console.ReadLine();
+
+                if (name.ToLower() == "exit")
+                {
+                    break;
+                }
+
+
+                var hobby = hobbyInstance.AskForHobby(name);
+                Console.WriteLine(hobby);
+            }
+        }
+    }
+}
+
---
+++ CsharpRandom/RandomGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpRandom
+{
+    internal class RandomGenerator
+    {
+        public string AskForHobby(string name)
+        {
+            Console.WriteLine($"Hello, {name}! This is your Chosen hobby:");
+            Random rand = new Random();
+            var randomNumber = rand.Next(0, 4);
+
+            string hobby;
+
+            if (randomNumber == 0)
+            {
+                hobby = $"{name} became a painter, you love painting birds in particular";
+            }
+            else if (randomNumber == 1)
+            {
+                hobby = $"Fish watcher, {name} enjoys watching fish swim around in ponds";
+            }
+            else if (randomNumber == 2)
+            {
+                hobby = $"{name} suddenly feels the urge to throw rocks around on a regular basis, big and small";
+            }
+            else if (randomNumber == 3)
+            {
+                hobby = $"The moon is calling {name}, you enjoy watching the sky at night";
+            }
+            else
+            {
+                hobby = $"Would you like to go again?";
+            }
+
+            return hobby;
+        }
+    }
+}
+
